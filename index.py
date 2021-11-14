b595c3f91145da243a28d00022fa4fd679a0d920
Anna Taylor <aetaylor7@outlook.com>
2021-11-13 19:49:40 -0500

    minor formatting change

--- index.py
+++ index.py
@@ -10,10 +10,11 @@ busters = [
 
 client = discord.Client()
 
-@client.event
 
+@client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
